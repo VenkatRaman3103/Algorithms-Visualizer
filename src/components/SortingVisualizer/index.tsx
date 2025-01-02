f8a99dfee4215812b4e9085a25927f46fb9bddd4
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-02 20:59:39 +0530

    added options tray toggle button

--- src/components/SortingVisualizer/index.tsx
+++ src/components/SortingVisualizer/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './index.scss'
+import { OptionTrayToggle } from '@/assets/OptionTrayToggle'
 
 export const SortingVisualizer: React.FC = (): React.JSX.Element => {
 	return (
 		<div className="sorting-visualizer-container">
-			<div></div>
+			<div className="options-tray-toggle">
+				<OptionTrayToggle />
+			</div>
 		</div>
 	)
 }
