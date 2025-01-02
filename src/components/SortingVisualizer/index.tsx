2c5bfac3a8346c9f542091e35e293fcaab921507
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-02 20:34:28 +0530

    base layout

---
+++ src/components/SortingVisualizer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+export const SortingVisualizer: React.FC = (): React.JSX.Element => {
+	return (
+		<div className="sorting-visualizer-container">
+			<div></div>
+		</div>
+	)
+}
