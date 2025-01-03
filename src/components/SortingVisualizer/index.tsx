86032f02db391a4db71dab884d53d4de6cdac9f6
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-03 22:29:36 +0530

    added strips for sorting

--- src/components/SortingVisualizer/index.tsx
+++ src/components/SortingVisualizer/index.tsx
@@ -7,22 +7,85 @@ import { OptionTrayToggle } from '@/assets/OptionTrayToggle'
 export const SortingVisualizer: React.FC = (): React.JSX.Element => {
 	const [isToggleActive, setIsToggleActive] = useState<boolean>(false)
 
+	const arr = [
+		22, 14, 6, 18, 9, 21, 3, 17, 11, 2, 20, 8, 15, 1, 19, 7, 16, 4, 13, 5, 10, 12, 21, 14, 5,
+		26, 7, 16, 4, 14, 6, 18, 9, 21, 3, 17, 11, 2, 20, 8, 15, 1,
+	]
+
 	return (
-		<div className="sorting-visualizer-container">
-			<div
-				className={`options-tray-toggle ${isToggleActive ? 'open-tray' : ''}`}
-				onMouseEnter={() => setIsToggleActive(true)}
-				onMouseLeave={() => setIsToggleActive(true)}
-			>
-				{!isToggleActive ? (
-					<div className={`btn-wrapper ${isToggleActive ? 'disolve' : ''}`}>
-						<OptionTrayToggle />
+		<>
+			<div className="sorting-visualizer-container">
+				<Tray setIsToggleActive={setIsToggleActive} isToggleActive={isToggleActive} />
+				<div className="sorting-preview-container">
+					<div className="sorting-preview-wrapper">
+						{arr.map((num) => (
+							<div
+								className={`sorting-strip`}
+								style={{ height: `calc(${num}0px + 300px)` }}
+							></div>
+						))}
 					</div>
-				) : (
-					<OptionsTray />
-				)}
+				</div>
 			</div>
+			<Buttons />
+		</>
+	)
+}
+
+type TrayProp = {
+	setIsToggleActive: React.Dispatch<React.SetStateAction<boolean>>
+	isToggleActive: boolean
+}
+
+export const Tray: React.FC<TrayProp> = ({
+	setIsToggleActive,
+	isToggleActive,
+}): React.JSX.Element => {
+	return (
+		<div
+			className={`options-tray-toggle ${isToggleActive ? 'open-tray' : ''}`}
+			onMouseEnter={() => setIsToggleActive(true)}
+			onMouseLeave={() => setIsToggleActive(true)}
+		>
+			{!isToggleActive ? (
+				<div className={`btn-wrapper ${isToggleActive ? 'disolve' : ''}`}>
+					<OptionTrayToggle />
+				</div>
+			) : (
+				<OptionsTray />
+			)}
 		</div>
+	)
+}
+
+export const Buttons: React.FC = (): React.JSX.Element => {
+	return (
+		<>
+			<div className="buttons-wrapper">
+				<div className="paus-and-play-btn">
+					<div className="btn-wrapper-play">
+						<div className="btn-icon"></div>
+						<div className="btn-label">Play</div>
+					</div>
+				</div>
+				<div className="shuffle-btn">
+					<div className="btn-wrapper-shuffle">
+						<div className="btn-icon"></div>
+						<div className="btn-label">Shuffle</div>
+					</div>
+				</div>
+				<div className="reset-btn">
+					<div className="btn-wrapper-reset">
+						<div className="btn-icon"></div>
+						<div className="btn-label">Reset</div>
+					</div>
+				</div>
+				<div className="delay-btn-wrapper">
+					<div className="delay-btn-slidder"></div>
+					<div className="delay-btn-label"></div>
+				</div>
+			</div>
+		</>
 	)
 }
 
