3cea2dd37267b81f2d86062ce13849641d2f50a7
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-19 01:30:22 +0530

    added toggle functionality to the code block

--- src/components/Algorithm/index.tsx
+++ src/components/Algorithm/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 
 export const Algorithm = () => {
+	const [isCodeBlockOpen, setIsCodeBlockOpen] = useState(true)
 	return (
 		<>
 			<div className="main-container">
 				<div>Algorithm</div>
 				<div className="code-block-container">
 					<div className="code-block-wrapper">
-						<div className="header-wrapper">
+						<div
+							className="header-wrapper"
+							onClick={() => setIsCodeBlockOpen(!isCodeBlockOpen)}
+						>
 							<div className="heading">Code instructions</div>
 							<div className="toggle-btn">V</div>
 						</div>
-						<div className="code-block"></div>
+						<div className={`code-block ${isCodeBlockOpen ? 'expand' : ''}`}></div>
 					</div>
 				</div>
 			</div>
