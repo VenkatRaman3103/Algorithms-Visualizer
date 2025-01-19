f2e96ea7b0505d5835c7850ab3cb7036d1f21655
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-01-19 13:32:46 +0530

    added heading with gradient effect and description

--- src/components/Algorithm/index.tsx
+++ src/components/Algorithm/index.tsx
@@ -17,32 +17,78 @@ export const Algorithm = () => {
 
 	return (
 		<div className="main-container">
-			<div>Algorithm</div>
-			<div className="code-block-container">
-				<div className="code-block-wrapper">
-					<div
-						className="header-wrapper"
-						onClick={() => setIsCodeBlockOpen(!isCodeBlockOpen)}
-					>
-						<div className="heading">Code instructions</div>
-						<div className="toggle-btn">V</div>
-					</div>
-					<div className={`code-block ${isCodeBlockOpen ? 'expand' : ''}`}>
-						{!isCodeBlockOpen && delayedOptionsVisible && (
-							<div className="language-switcher-container">
-								<div className="language-switcher-wrapper">
-									{Array.from({ length: 5 }).map((item, ind) => (
-										<LanguageSelector
-											key={ind}
-											selectedLanguage={selectedLanguage}
-											setSelectedLanguage={setSelectedLanguage}
-											ind={ind}
-										/>
-									))}
-								</div>
+			<IntroSection />
+			<CodeBlock
+				setIsCodeBlockOpen={setIsCodeBlockOpen}
+				isCodeBlockOpen={isCodeBlockOpen}
+				delayedOptionsVisible={delayedOptionsVisible}
+				selectedLanguage={selectedLanguage}
+				setSelectedLanguage={setSelectedLanguage}
+			/>
+		</div>
+	)
+}
+
+export const IntroSection = () => {
+	return (
+		<div className="intro-section-container">
+			<div className="intro-section-wrapper">
+				<div className="algorithm-title">Binary Search</div>
+
+				<div className="description">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+					perferendis Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+					Consequatur
+				</div>
+
+				<TimeComplexity />
+
+				<div className="divider"></div>
+			</div>
+		</div>
+	)
+}
+
+export const TimeComplexity = () => {
+	return (
+		<div className="time-container">
+			<div className="time-wrapper">TimeComplexity</div>
+		</div>
+	)
+}
+
+export const CodeBlock = ({
+	setSelectedLanguage,
+	setIsCodeBlockOpen,
+	isCodeBlockOpen,
+	delayedOptionsVisible,
+	selectedLanguage,
+}) => {
+	return (
+		<div className="code-block-container">
+			<div className="code-block-wrapper">
+				<div
+					className="header-wrapper"
+					onClick={() => setIsCodeBlockOpen(!isCodeBlockOpen)}
+				>
+					<div className="heading">Code instructions</div>
+					<div className="toggle-btn">V</div>
+				</div>
+				<div className={`code-block ${isCodeBlockOpen ? 'expand' : ''}`}>
+					{!isCodeBlockOpen && delayedOptionsVisible && (
+						<div className="language-switcher-container">
+							<div className="language-switcher-wrapper">
+								{Array.from({ length: 5 }).map((item, ind) => (
+									<LanguageSelector
+										key={ind}
+										selectedLanguage={selectedLanguage}
+										setSelectedLanguage={setSelectedLanguage}
+										ind={ind}
+									/>
+								))}
 							</div>
-						)}
-					</div>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
